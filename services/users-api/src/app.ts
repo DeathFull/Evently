6c87470338e7e310d6915f728bd84608c3ebea2e
Edderkoppen <cartron.mathieu37@gmail.com>
2025-03-22 18:42:42 +0100

    feat: Add CORS, Swagger documentation, and user routes to Express app

--- services/users-api/src/app.ts
+++ services/users-api/src/app.ts
@@ -1,18 +1,41 @@
 import express, {
-	type NextFunction,
-	type Request,
-	type Response,
+  type NextFunction,
+  type Request,
+  type Response,
 } from "express";
+import UserRouter from "./routers/UserRouter";
+import cors from "cors";
+import fs from "node:fs/promises";
+import { parse } from "yaml";
+import swaggerUi from "swagger-ui-express";
 
 export const app = express();
+const swaggerDocument = parse(await fs.readFile("./swagger.yaml", "utf8"));
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello from Express 5 with TypeScript and tsx!");
-});
+const allowedOrigins = ["http://localhost:3000", "*"];
 
+app.use(
+  cors({
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/users", UserRouter);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-	console.error(err.stack);
-	res.status(500).send("Something broke!");
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
