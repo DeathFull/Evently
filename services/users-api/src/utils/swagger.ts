d4652cadcd702b38659086290ae4eafd45201162
Edderkoppen <cartron.mathieu37@gmail.com>
2025-03-22 18:48:33 +0100

    refactor: Update Swagger document path in loadSwaggerDocument function

--- services/users-api/src/utils/swagger.ts
+++ services/users-api/src/utils/swagger.ts
@@ -1,13 +1,9 @@
 import fs from "node:fs/promises";
 import { parse } from "yaml";
 
-/**
- * Loads and parses the Swagger YAML file
- * @returns Promise that resolves to the parsed Swagger document
- */
 export async function loadSwaggerDocument() {
   try {
-    const yamlContent = await fs.readFile("./swagger.yaml", "utf8");
+    const yamlContent = await fs.readFile("../swagger.yaml", "utf8");
     return parse(yamlContent);
   } catch (error) {
     console.error("Error loading Swagger document:", error);
