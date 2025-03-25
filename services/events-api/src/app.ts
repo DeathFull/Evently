b11a455e2d6704b1d57837c81615ba9e4aca1158
Edderkoppen <cartron.mathieu37@gmail.com>
2025-03-25 12:15:51 +0100

    fix: fix biome

--- services/events-api/src/app.ts
+++ services/events-api/src/app.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
 import express, {
-  type NextFunction,
-  type Request,
-  type Response,
+	type NextFunction,
+	type Request,
+	type Response,
 } from "express";
 import swaggerUi from "swagger-ui-express";
-import { loadSwaggerDocument } from "./utils/swagger";
 import EventRouter from "./routers/EventRouter";
+import { loadSwaggerDocument } from "./utils/swagger";
 
 export const app = express();
 
@@ -15,33 +15,33 @@ app.use(express.json());
 const allowedOrigins = ["http://localhost:3000", "*"];
 
 app.use(
-  cors({
-    origin: (
-      origin: string | undefined,
-      callback: (err: Error | null, allow?: boolean) => void,
-    ) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    allowedHeaders: ["Authorization", "Content-Type"],
-  }),
+	cors({
+		origin: (
+			origin: string | undefined,
+			callback: (err: Error | null, allow?: boolean) => void,
+		) => {
+			if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+		credentials: true,
+		allowedHeaders: ["Authorization", "Content-Type"],
+	}),
 );
 
 let swaggerDocument = null;
 loadSwaggerDocument()
-  .then((document) => {
-    swaggerDocument = document;
-    app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  })
-  .catch((error) => {
-    console.error("Failed to load Swagger document:", error);
-  });
+	.then((document) => {
+		swaggerDocument = document;
+		app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+	})
+	.catch((error) => {
+		console.error("Failed to load Swagger document:", error);
+	});
 app.use("/events", EventRouter);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
 });
--- services/events-api/src/types.ts
+++ services/events-api/src/types.ts
@@ -1,16 +1,16 @@
 export interface Event {
-  id: string;
-  type: "CONCERT" | "SPECTACLE" | "FESTIVAL";
-  date: Date;
-  price: number;
-  location: string;
-  name: string;
+	id: string;
+	type: "CONCERT" | "SPECTACLE" | "FESTIVAL";
+	date: Date;
+	price: number;
+	location: string;
+	name: string;
 }
 
 export interface EventUpdate {
-  type?: "CONCERT" | "SPECTACLE" | "FESTIVAL";
-  date?: Date;
-  price?: number;
-  location?: string;
-  name?: string;
+	type?: "CONCERT" | "SPECTACLE" | "FESTIVAL";
+	date?: Date;
+	price?: number;
+	location?: string;
+	name?: string;
 }
